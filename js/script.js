c27560f25e45c7068fa77b66aaa3717bb3fba095
Tim <choitim2010@gmail.com>
2021-10-09 21:42:42 -0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,205 @@
+// "use strict";
+// console.log('hello wolrd');
+
+// let number = 'timur';
+// console.log( number);
+// const isProgmamer = true;
+// let age = 41;
+// console.log('age');
+// const firstName = 'Tumur'
+// const lastName = 'Tsoi'
+// var God = 'love'
+// console.log('God');
+// var familyName = 'true'
+// console.log('who loves me:' + firstName  a my age');
+// console.log('familyName')
+// const age = 43;
+// const currentYear = 2020
+// const birDay = 1977
+// const lastName = promt( message: 'input name')
+// alert(firstName + '  ' + lastName)
+// alert(12);
+// number = "sara";
+// console.log(number);
+// const obj = {
+//     name: "john",
+//     age: 25,
+//     sex: "man",
+//     isMarried : false,
+// };
+// console.log( obj.isMarried) ;
+let arr = [ 'plg.ong', 'flower.png', [ 'one', "two", 3432,],{    name: "john",
+age: 25, sex: "man", isMarried : false},143423];
+console.log(arr[3] );
+    // alert( "hello");
+    // const result = confirm(" how old are you");
+    // console.log(result);
+    // const answer = +prompt(" сколько вам лет", "");
+    // // const age = prompt(" сколько вам дают люди", "");
+    // console.log(answer+1001111); 
+    // console.log(typeof(age));
+    // const answer =[];
+    // answer[0]=prompt("сколько вам лет", "");
+    // answer[1]=prompt("сколько времени", "");
+    // answer[2]=prompt("сколько что там", "");
+    // answer[3]=prompt("сколько далеко", "");
+    //  document.write(answer);
+    // let myTest = prompt( 'ну как тебе такое ? ' , "");
+    // if (myTest == 15){
+    //     alert( "you are best");
+        
+    // }else{
+    //     alert("give them self more time");}
+    //     let position=prompt("кто ты по жизни?", '');
+    //     alert(`чо крутой ${position}  по жизни?`);
+// let answers = [];
+// answers[0] = prompt("what your name?" , "");
+// answers[1] = prompt("what your чотто?" , "");
+// answers[2] = prompt("what your ышчу?" , "");
+// console.log(answers);
+не работает не знаю почему
+// const woman = {
+//     age: 45,
+//     husben:'john',
+//     color: 'red', 'blue', 'green',
+//     kids: 'tom', 'sarra',
+//     // marrige: true,
+// };
+// console.log( woman. age);
+                        //                 вот этот код работает
+                        //  const obj = {
+                        //         name: "john",
+                        //         age: 25,
+                        //         sex: "man",
+                        //         isMarried : false,
+                        //     };
+                        //     console.log( obj['age']) ; 
+const house= {
+    price: 20230402,
+    ddata: false,
+    car: "honda",
+    red: 'bluer',
+    kids:"ola",
+    dfg:[12,'kdvmv', false],
+};
+console.log(house['car']);
+
+   const car={
+       price:1200,
+       dob: "june",
+       ingin: false,
+       sets: 1,
+       capaciti:[12,'opo',false],
+   };
+   console.log(car['capaciti']);
+
+
+ let num = 500;
+ if( num>501){
+     console.log( " not enaf");
+
+ }else if (num<400){
+     console.log(" more");
+
+ }else if(num=500){
+     console.log("ok");
+ }
+ else {console.log("123");}
+
+ const num=200;
+ (num>300) ? console.log("ok") : console.log("again");
+
+  const leter = 3;
+ if ( leter>15){
+     console.log("ok");
+ }else if(leter===33){
+     console.log("opo");
+ }else{
+     console.log("haha");
+ }
+ (leter<2) ? console.log("mslo") : console.log("opa");
+  const leter = 3;
+  switch(leter){
+      case 2:
+          console.log("ooo");
+          break;
+          case 4:
+              console.log("222");
+              break;
+              case 6:
+                  console.log("444");
+                  break;
+                  default:
+                      console.log(" opa");
+                      case 3:
+                          console.log(" ok");
+                          break;}
+
+
+                          const tim = 44;
+                          if(tim>100){
+                              console.log("good");
+                          }else if(tim<45){
+                              console.log("less");
+                          }else{
+                              console.log("try");
+                          }
+                          (tim>45) ? console.log("less") : console.log("try");
+                          Object
+//  const dada={
+//      age: 65,
+//      car: "nissan",
+//      wife:"roza",
+//      lotOfmany: false,
+    
+//  };
+//  console.log(dada.age);
+ let dada =1000;
+//  if(dada==64){
+//      console.log("less");
+//  }else if( dada == 65){
+//      console.log("ok");
+//  }else if( dada>2000){
+//      console.log("opa");
+// }else{
+//     console.log("111");
+// }
+let ooo =1009;
+switch(ooo){
+    case 2000:
+        console.log("less");
+        break;
+        case 1000:
+            console.log("ok");
+            break;
+            default:
+                console.log("tyr");
+                break;
+}
+
+for(let  i=1;i<9;i++){
+    if (i===5){
+        break;
+    }
+    console.log(i);
+    
+}
+for(let i = 0;i < 1; i++) {
+    const numberOfFilms = +prompt('Сколько фильмов вы уже смотрели?');
+    console.log(numberOfFilms);
+    if(numberOfFilms == "" || numberOfFilms == null){
+      alert('Строка не может быть пустой!');
+      i--;
+    }else if (numberOfFilms < 10 ){
+      console.log('Просмотрено очень мало фильмов');
+    }else if (numberOfFilms >= 10 && numberOfFilms <= 30 ) {
+      console.log('Вы класический Зритель');
+    }else if(numberOfFilms> 30 ){
+      console.log('Вы киноман');
+    }
+  }
+
+
+   
+ 
+  
